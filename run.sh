712904e17ecdab108d0a7d61539e0b68313394ff
Carlos Ignacio Pérez Sechi <external.z21csech@decathlon.com>
2016-01-19 12:21:34 +0100

    Changed the docker host:port syntax

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo $1
-docker run -d --name=bind --dns=127.0.0.1 --publish=$1:53/udp --publish=$1:10000:10000 --volume=$PWD/data:/data --env='ROOT_PASSWORD=password' sameersbn/bind:latest
+docker run -d --name=bind --dns=127.0.0.1 --publish=$1:53:53/udp --publish=$1:10000:10000 --volume=$PWD/data:/data --env='ROOT_PASSWORD=password' sameersbn/bind:latest
 
 exit 0
